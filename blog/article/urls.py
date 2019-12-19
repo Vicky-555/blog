78242136ba6946b96452f47c8bdd8c7d7f3d9208
Vicky-555 <s10714104@gm.cyut.edu.tw>
2019-12-19 18:28:07 +0800

    final finish

--- blog/article/urls.py
+++ blog/article/urls.py
@@ -5,5 +5,8 @@ from article import views
 app_name = 'article'
 urlpatterns = [
     path('', views.article, name='article'),
+    path('articleCreate/', views.articleCreate, name='articleCreate'),
+    path('articleRead/<int:articleId>/', views.articleRead, name='articleRead'),
+    path('articleUpdate/<int:articleId>/', views.articleUpdate, name='articleUpdate'),
 ]
 
